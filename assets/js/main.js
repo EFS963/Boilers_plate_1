a9a24e9c342704f4ff03a59c7fb27bb42d078589
EFS963 <erickflores.s963@gmail.com>
2022-10-21 01:38:08 -0500

    commits modify

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,4 @@
-console.log("Entro al main.js");
-
+console.log("Entro al main.js")
 var myHeaders = new Headers();
 myHeaders.append("apikey", "tph4C1O9tNLexo1DwXLmWWw4WRcBKVkB");
 
@@ -7,9 +6,12 @@ var requestOptions = {
   method: 'GET',
   redirect: 'follow',
   headers: myHeaders
-};
+}
 
-fetch("https://api.apilayer.com/fixer/convert?to={to}&from={from}&amount={amount}", requestOptions)
-  .then(response => response.text())
-  .then(result => console.log(result))
+//fetch("https://api.apilayer.com/fixer/symbols", requestOptions)
+    fetch("https://api.apilayer.com/fixer/latest?symbols=MXN,EUR,GBP,CAD,KWD,GTQ,CNY&base=USD", requestOptions)//
+    //fetch("https://api.apilayer.com/fixer/convert?base=USD&symbols=EUR", requestOptions)
+  .then(response => response.json())
+  .then(data => console.log(data))
   .catch(error => console.log('error', error));
+
