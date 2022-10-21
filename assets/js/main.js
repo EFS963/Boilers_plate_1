ce1284b1306e08c83cc55a07eb332b2fb8389931
EFS963 <erickflores.s963@gmail.com>
2022-10-21 00:06:30 -0500

    commints js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,15 @@
 console.log("Entro al main.js");
+
+var myHeaders = new Headers();
+myHeaders.append("apikey", "tph4C1O9tNLexo1DwXLmWWw4WRcBKVkB");
+
+var requestOptions = {
+  method: 'GET',
+  redirect: 'follow',
+  headers: myHeaders
+};
+
+fetch("https://api.apilayer.com/fixer/convert?to={to}&from={from}&amount={amount}", requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
